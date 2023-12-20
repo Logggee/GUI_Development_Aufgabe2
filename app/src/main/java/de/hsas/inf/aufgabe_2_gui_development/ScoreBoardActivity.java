62180acae6aab0ad55b4fd3d6f83360cfd7bcb76
Jan Holderied <holderja@hs-albsig.de>
2023-12-20 20:30:03 +0100

    every thing working as it needs to

--- app/src/main/java/de/hsas/inf/aufgabe_2_gui_development/ScoreBoardActivity.java
+++ app/src/main/java/de/hsas/inf/aufgabe_2_gui_development/ScoreBoardActivity.java
@@ -7,7 +7,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,23 +21,34 @@ public class ScoreBoardActivity extends AppCompatActivity {
 
     private Button buttonDelete;
     private FileIOScores fileIO = new FileIOScores(this);
+    private ArrayAdapter<String> model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayList<ScoreItem> scoreItems = new ArrayList<ScoreItem>();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.score_board);
 
+        //create toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //get list view
+        ListView listView = findViewById(android.R.id.list);
 
         //Listener for delete Button, deletes all listet scores in the view and the score file
         buttonDelete = findViewById(R.id.deleteScores);
         buttonDelete.setOnClickListener(view->{
             fileIO.eraseContent("File1.txt");
+            model.clear();
+            model.notifyDataSetChanged();
         });
 
         //Read Content of the txt scores file
-        readData();
+        scoreItems = readScores();
+        //Creat the model and the list view items for score list
+        model = createListItems(scoreItems, listView);
     }
 
     //Methode is called on app startup to inflate the menu
@@ -57,16 +70,21 @@ public class ScoreBoardActivity extends AppCompatActivity {
         return false;
     }
 
-    private ArrayList<ScoreItem> readData(){
+    private ArrayList<ScoreItem> readScores(){
         fileIO.printFileContent("File1.txt");
         ArrayList<ScoreItem> scoreItems = new ArrayList<ScoreItem>();
         scoreItems = fileIO.readScores("File1.txt");
-        Iterator<ScoreItem> iterator = scoreItems.iterator();
-        Log.i("Results", scoreItems.toString());
-        while(iterator.hasNext()){
-            Log.i("Results", "Test");
-            Log.i("Results", iterator.next().toString());
-        }
         return scoreItems;
+    }
+
+    private ArrayAdapter<String> createListItems(ArrayList<ScoreItem> scoreItems, ListView listView){
+        ArrayList<String> listItems = new ArrayList<String>();
+        Iterator<ScoreItem> iterator = scoreItems.iterator();
+        while(iterator.hasNext()){
+            listItems.add("\n" + iterator.next().toString());
+        }
+        ArrayAdapter<String> model = new ArrayAdapter<String>(this, R.layout.list_items, listItems);
+        listView.setAdapter(model);
+        return model;
     }
 }
